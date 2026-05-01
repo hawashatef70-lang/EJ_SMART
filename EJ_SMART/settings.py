b928be42facfbcc86441b54d9288d91abd7dea56
Hawash <hawash@gmail.com>
2026-05-01 11:18:44 +0200

    disable auth temporarily

--- EJ_SMART/settings.py
+++ EJ_SMART/settings.py
@@ -187,21 +187,27 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+#REST_FRAMEWORK = {
+    #'DEFAULT_AUTHENTICATION_CLASSES': (
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
+   # ),
+
+    #'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+   # 'PAGE_SIZE': 10,
+
+   # 'DEFAULT_FILTER_BACKENDS': (
+      #  'django_filters.rest_framework.DjangoFilterBackend',
+   # ),
+
+   # 'EXCEPTION_HANDLER': 'core.exceptions.custom_exception_handler',
+#}
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
-
-    'EXCEPTION_HANDLER': 'core.exceptions.custom_exception_handler',
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
 
 SIMPLE_JWT = {
